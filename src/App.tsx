181448f2981eb84e5f585ee421c1b85a009a68f0
Guilherme Viana <imluizguilara@hotmail.com>
2023-09-08 10:32:28 -0300

    feat: Visual structure done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Provider as ReduxProvider } from "react-redux";
+
+import { Player } from "./pages/player";
+import { store } from "./store";
+
+export function App() {
+	return (
+		<ReduxProvider store={store}>
+			<Player />
+		</ReduxProvider>
+	);
+}
